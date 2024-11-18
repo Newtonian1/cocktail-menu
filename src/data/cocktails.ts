90f1f235780e33ae632bbb5ecda05ef99311f02b
Newtonian1 <ross3jh@gmail.com>
2024-11-18 12:07:53 -0500

    added paper plane

--- src/data/cocktails.ts
+++ src/data/cocktails.ts
@@ -6,69 +6,9 @@ export interface Cocktail {
 
 export const cocktails: Cocktail[] = [
     {
-        name: "Old Fashioned",
-        ingredients: ["bourbon", "angostura", "simple"],
-        image: "old_fashioned"
-    },
-    {
-        name: "Boulevardier",
-        ingredients: ["bourbon", "campari", "sweet vermouth"],
-        image: "boulevardier"
-    },
-    {
-        name: "Whiskey Sour",
-        ingredients: ["bourbon", "lemon", "simple", "egg white"],
-        image: "whiskey_sour"
-    },
-    {
-        name: "Manhattan",
-        ingredients: ["rye", "sweet vermouth", "angostura", "regans"],
-        image: "manhattan"
-    },
-    {
-        name: "Sazerac",
-        ingredients: ["absinthe", "rye", "cognac", "angostura", "peychauds" ,"simple"],
-        image: "sazerac"
-    },
-    {
-        name: "Old Pal",
-        ingredients: ["rye", "campari", "dry vermouth"],
-        image: "old_pal"
-    },
-    {
-        name: "Ward Eight",
-        ingredients: ["rye", "lemon", "orange", "grenadine"],
-        image: "ward_eight"
-    },
-    {
-        name: "Daiquiri",
-        ingredients: ["rum", "lime", "simple"],
-        image: "daiquiri"
-    }, 
-    {
-        name: "Dark 'N Stormy",
-        ingredients: ["rum", "lime", "ginger beer"],
-        image: "dark_n_stormy"
-    }, 
-    {
-        name: "Jungle Bird",
-        ingredients: ["rum", "campari", "pineapple", "lime", "simple"],
-        image: "jungle_bird"
-    }, 
-    {
-        name: "Negroni",
-        ingredients: ["gin", "campari", "sweet vermouth"],
-        image: "negroni"
-    }, 
-    {
-        name: "Martini",
-        ingredients: ["gin", "dry vermouth"],
-        image: "martini"
-    },
-    {
-        name: "Tom Collins",
-        ingredients: ["gin", "lemon", "simple", "club soda"],
-        image: "tom_collins"
+        name: "Americano",
+        ingredients: ["campari", "sweet vermouth", "club soda"],
+        image: "americano"
     },
     {
         name: "Aviation",
@@ -76,59 +16,9 @@ export const cocktails: Cocktail[] = [
         image: "aviation"
     },
     {
-        name: "Gimlet",
-        ingredients: ["gin", "lime", "simple"],
-        image: "gimlet"
-    },
-    {
-        name: "Margarita",
-        ingredients: ["tequila", "lime", "orange liqueur", "agave"],
-        image: "margarita"
-    },
-    {
-        name: "Paloma",
-        ingredients: ["tequila", "lime", "grapefruit", "campari"],
-        image: "paloma"
-    },
-    {
-        name: "Tequila Sunrise",
-        ingredients: ["tequila", "orange", "grenadine"],
-        image: "tequila_sunrise"
-    },
-    {
-        name: "Division Bell",
-        ingredients: ["mezcal", "lime", "aperol", "luxardo"],
-        image: "division_bell"
-    },
-    {
-        name: "Moscow Mule",
-        ingredients: ["vodka", "lime", "ginger beer"],
-        image: "moscow_mule"
-    },
-    {
-        name: "Cosmopolitan",
-        ingredients: ["vodka", "lime", "cranberry", "orange liqueur"],
-        image: "cosmopolitan"
-    },
-    {
-        name: "Espresso Martini",
-        ingredients: ["vodka", "coffee liqueur", "coffee", "simple"],
-        image: "espresso_martini"
-    },
-    {
-        name: "Lemon Drop",
-        ingredients: ["vodka", "lemon", "orange liqueur", "simple"],
-        image: "lemon_drop"
-    },
-    {
         name: "Bay Breeze",
         ingredients: ["vodka", "cranberry", "pineapple"],
         image: "bay_breeze"
-    },
-    {
-        name: "Sidecar",
-        ingredients: ["cognac", "lemon", "orange liqueur"],
-        image: "sidecar"
     },
     {
         name: "Between the Sheets",
@@ -136,13 +26,128 @@ export const cocktails: Cocktail[] = [
         image: "between_the_sheets"
     },
     {
-        name: "Americano",
-        ingredients: ["campari", "sweet vermouth", "club soda"],
-        image: "americano"
+        name: "Boulevardier",
+        ingredients: ["bourbon", "campari", "sweet vermouth"],
+        image: "boulevardier"
+    },
+    {
+        name: "Cosmopolitan",
+        ingredients: ["vodka", "lime", "cranberry", "orange liqueur"],
+        image: "cosmopolitan"
+    },
+    {
+        name: "Daiquiri",
+        ingredients: ["rum", "lime", "simple"],
+        image: "daiquiri"
+    },
+    {
+        name: "Dark 'N Stormy",
+        ingredients: ["rum", "lime", "ginger beer"],
+        image: "dark_n_stormy"
+    },
+    {
+        name: "Division Bell",
+        ingredients: ["mezcal", "lime", "aperol", "luxardo"],
+        image: "division_bell"
+    },
+    {
+        name: "Espresso Martini",
+        ingredients: ["vodka", "coffee liqueur", "coffee", "simple"],
+        image: "espresso_martini"
+    },
+    {
+        name: "Gimlet",
+        ingredients: ["gin", "lime", "simple"],
+        image: "gimlet"
+    },
+    {
+        name: "Jungle Bird",
+        ingredients: ["rum", "campari", "pineapple", "lime", "simple"],
+        image: "jungle_bird"
+    },
+    {
+        name: "Lemon Drop",
+        ingredients: ["vodka", "lemon", "orange liqueur", "simple"],
+        image: "lemon_drop"
+    },
+    {
+        name: "Manhattan",
+        ingredients: ["rye", "sweet vermouth", "angostura", "regans"],
+        image: "manhattan"
+    },
+    {
+        name: "Margarita",
+        ingredients: ["tequila", "lime", "orange liqueur", "agave"],
+        image: "margarita"
+    },
+    {
+        name: "Martini",
+        ingredients: ["gin", "dry vermouth"],
+        image: "martini"
+    },
+    {
+        name: "Moscow Mule",
+        ingredients: ["vodka", "lime", "ginger beer"],
+        image: "moscow_mule"
+    },
+    {
+        name: "Negroni",
+        ingredients: ["gin", "campari", "sweet vermouth"],
+        image: "negroni"
+    },
+    {
+        name: "Old Fashioned",
+        ingredients: ["bourbon", "angostura", "simple"],
+        image: "old_fashioned"
+    },
+    {
+        name: "Old Pal",
+        ingredients: ["rye", "campari", "dry vermouth"],
+        image: "old_pal"
+    },
+    {
+        name: "Paloma",
+        ingredients: ["tequila", "lime", "grapefruit", "campari"],
+        image: "paloma"
+    },
+    {
+        name: "Paper Plane",
+        ingredients: ["bourbon", "aperol", "amaro nonino", "lemon"],
+        image: "paper_plane"
+    },
+    {
+        name: "Sazerac",
+        ingredients: ["absinthe", "rye", "cognac", "angostura", "peychauds", "simple"],
+        image: "sazerac"
+    },
+    {
+        name: "Sidecar",
+        ingredients: ["cognac", "lemon", "orange liqueur"],
+        image: "sidecar"
+    },
+    {
+        name: "Tequila Sunrise",
+        ingredients: ["tequila", "orange", "grenadine"],
+        image: "tequila_sunrise"
+    },
+    {
+        name: "Tom Collins",
+        ingredients: ["gin", "lemon", "simple", "club soda"],
+        image: "tom_collins"
     },
     {
         name: "Vermouth Cocktail",
         ingredients: ["sweet vermouth", "luxardo", "angostura"],
         image: "vermouth_cocktail"
     },
+    {
+        name: "Ward Eight",
+        ingredients: ["rye", "lemon", "orange", "grenadine"],
+        image: "ward_eight"
+    },
+    {
+        name: "Whiskey Sour",
+        ingredients: ["bourbon", "lemon", "simple", "egg white"],
+        image: "whiskey_sour"
+    }
 ]
